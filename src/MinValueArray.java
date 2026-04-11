dfe59037f9770d9e641342294e234dd413ca2e06
jawad <muhammadjawadchandio@gmail.com>
2026-04-12 00:32:21 +0500

    MinValue and Sort

--- src/MinValueArray.java
+++ src/MinValueArray.java
@@ -1,7 +1,15 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class MinValueArray {
    public static void main(String[] args) {
        int[] a = {5,5,6,7,876,1,44};
        int min =a[0];
+       System.out.println("before sort");
+       for (int i =0; i<a.length;i++){
+           System.out.print(a[i]+" ");
+       }
+       System.out.println("Min Value of Array");
        for (int i = 0; i < a.length;i++){
            if (a[i]<min){
                min=a[i];
@@ -10,7 +18,15 @@ public class MinValueArray {
 
 
        }
+       Arrays.sort(a);
        System.out.println(min);
+       System.out.println("After sort");
+       for (int i =0; i<a.length;i++){
+
+           System.out.print(a[i]+" ");
+       }
+
+
 
     }
 }
