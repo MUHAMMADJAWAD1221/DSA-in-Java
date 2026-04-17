e7c1b600461c9971b8a42fda78f87a191fa1e21e
jawad <muhammadjawadchandio@gmail.com>
2026-04-18 00:32:00 +0500

    perfect code with two outputs First Largest && Second largest

--- Array DS/SecondlargestNum.java
+++ Array DS/SecondlargestNum.java
@@ -1,7 +1,7 @@
 public class SecondlargestNum {
    public static void main(String[] args) {
 
-       // PENDING BECUSE OF AN ERROR
+
        int[] arr={1,2,3,4,76,45};
        int Max = Integer.MIN_VALUE;
        int max= Integer.MIN_VALUE;
@@ -11,12 +11,14 @@ public class SecondlargestNum {
            }
           for (i =0; i<arr.length;i++){
                if (arr[i]>Max && arr[i]!=max){
-                   max = arr[i];
+                   Max = arr[i];
                }
-              System.out.println(max);
+
+
            }
        }
-
+       System.out.println(" first largest Value "+Max);
+       System.out.println("Second largest value "+max);
 
     }
 
